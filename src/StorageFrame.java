a6c9ec221146cd2df09b51d87a18964aad58e48a
KKOULY <dmt.solovyov@gmail.com>
2020-06-19 18:12:16 +0300

    fix

--- src/StorageFrame.java
+++ src/StorageFrame.java
@@ -1,5 +1,8 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class StorageFrame extends JFrame {
     public static final int WIDTH = 900;
@@ -11,6 +14,7 @@ public class StorageFrame extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
         this.setIconImage(new ImageIcon("storageIcon.png").getImage());
+        this.setContentPane(new BgPanel());
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
         this.setBounds((int)dimension.getWidth()/2-WIDTH/2,(int)dimension.getHeight()/2-HEIGHT/2,WIDTH,HEIGHT);
         initAllElements();
@@ -20,6 +24,16 @@ public class StorageFrame extends JFrame {
 
     private void initAllElements() {
         this.setJMenuBar(new MyMenu(this));
+    }
+
+    class BgPanel extends JPanel{
+        public void paintComponent(Graphics g){
+            Image im = null;
+            try {
+                im = ImageIO.read(new File("background.png"));
+            } catch (IOException e) {}
+            g.drawImage(im, 0, 0, null);
+        }
     }
 
     public static void main(String[] args) {
